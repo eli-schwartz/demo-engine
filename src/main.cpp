419cef279a015bf1f2cde08c9a54805a77581093
redve <106469071+redve-dev@users.noreply.github.com>
2022-08-28 16:23:56 +0200

    create demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
-#include "test.hpp"
+#include "Engine/Engine.hpp"
+#include "Ball/Ball.hpp"
 
 int main(){
-	Foo();
-
-	return 0;
+	auto e = std::make_unique<Engine>(800, 600);
+	auto c = std::make_shared<Ball>(glm::vec2(400, 300), 10);
+	e->PushObject(c);
+	for(int i=0; i<10; i++){
+		e->Draw();
+		c->Move(glm::vec2(10,0));
+		SDL_Delay(100);
+	}
 }
